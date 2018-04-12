ea1753fc12d9bd4deac863f1f18e29bd6d76b61c
Rafael Odon de Alencar <rafael.odon@serpro.gov.br>
2018-04-12 12:47:20 -0300

    Commiting angular build output in node public folder. Removing some lines to get a lean version of app.js.

--- node/app.js
+++ node/app.js
@@ -1,16 +1,12 @@
-var createError = require('http-errors');
+//Imports
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var tasksRouter = require('./routes/tasks');
 
+//App
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,8 +14,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Add headers
-app.use(function (req, res, next) {  
+// CORS for Angular development mode
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -28,21 +24,5 @@ app.use(function (req, res, next) {
 });
 
 app.use('/tasks', tasksRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  console.log(err);
-});
 
 module.exports = app;
